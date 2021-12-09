69a2279cf46d0a690a7ae568c85b0e34e14fe68a
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-09 12:21:24 +0500

    Added js event listner

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const gridContainer = document.querySelector('.grid-container');
 
-for(let i = 1; i<257; i++) {
+for(let i = 0; i<256; i++) {
     const grid = document.createElement('div');
     grid.setAttribute('class', 'grid');
     gridContainer.appendChild(grid);
+
+    grid.addEventListener('mouseenter', (e) => {
+        grid.style.backgroundColor = "black"
+    })
 }
