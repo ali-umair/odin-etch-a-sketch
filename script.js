ad9914419850cfe78b9459a5be4e9665ea98b937
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-09 12:58:27 +0500

    Generating grid by user input size

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const gridContainer = document.querySelector('.grid-container');
+const root = document.querySelector(':root');
+const columnSize = +prompt("Enter the size of column (1-100):");
 
-for(let i = 0; i<256; i++) {
+
+// var rootStyles = getComputedStyle(root);
+// var columnSize1= rootStyles.getPropertyValue('--column');    --to fetch CSS Variables
+// console.log(columnSize1); 
+
+
+root.style.setProperty('--column', `${columnSize}`);
+
+
+for(let i = 0; i<columnSize*columnSize; i++) {
     const grid = document.createElement('div');
     grid.setAttribute('class', 'grid');
     gridContainer.appendChild(grid);
